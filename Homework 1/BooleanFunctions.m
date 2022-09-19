577a8aefdefac65a2a634d9d6d4c9aa100c37324
maximiliann97 <maximilian@live.se>
2022-09-19 12:43:02 +0200

    code added to boolean functions

--- Homework 1/BooleanFunctions.m
+++ Homework 1/BooleanFunctions.m
@@ -3,7 +3,7 @@ clear all;close all;clc
 
 
 % Initialization
-n = [2 3 4 5];
+dimensions = [2 3 4 5];
 nTrials = 1e4;
 nEpochs = 20;
 eta = 0.05;
@@ -12,7 +12,32 @@ counter = 0;
 booleanInputs = [0 1 0 1;0 0 1 1]';
 
 usedBool = [];
+for n = 1:length(dimensions)
+    for trial = 1:nTrials
+        booleanOutputs = randi([0, 1], 2^n, 1);
+        booleanOutputs(booleanOutputs==0) = -1;
+    
+        if booleanOutputs 
+           w = randn(n) * 1/sqrt(n);
+           theta = 0;
+        end
+    end
 
-for trial = 1:nTrials
+    for epoch = 1:nEpochs
+        totalError = 0;
+        for i = 2^n
+            y = sign();
+            error = booleanOutputs(i) - y;
+            
+            % update w and theta
+            
+
+        end
+        if totalError == 0
+            counter = counter + 1;
+            break
+        end
+    end
+    % add booleanOutputs to usedBool
     
 end
