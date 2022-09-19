87d45971d241d2c3b6ef05ee43669c391241d0a1
maximiliann97 <maximilian@live.se>
2022-09-19 15:37:22 +0200

    boolean function code

--- Homework 1/BooleanFunctions.m
+++ Homework 1/BooleanFunctions.m
@@ -3,46 +3,58 @@ clear all;close all;clc
 
 
 % Initialization
-dimension = [2 3 4 5];
+n = 2;
 nTrials = 1e4;
 nEpochs = 20;
 eta = 0.05;
 counter = 0;
 
-booleanInputs = [0 1 0 1;0 0 1 1]';
-
-usedBool = [1 1 1 1]';
+booleanInputs = dec2bin(0:2^n-1)' - '0';
+booleanInputs(booleanInputs==0) = -1;
+usedBool = [];
 index = 1;
 
-for n = 1:length(dimension)
+%for n = 1:length(dimension)
     for trial = 1:nTrials
-        booleanOutputs = randi([0, 1], 2^(dimension(n)), 1);
+        booleanOutputs = randi([0, 1], 2^n, 1);
         booleanOutputs(booleanOutputs==0) = -1;
+        o = 0;
+        
+        for b = 1:size(usedBool,2)
+           if isequal(usedBool(:,b),booleanOutputs)
+               o = 1;
+           end
+        end
+
+        if o == 0
+               w = randn(1,n) * 1/sqrt(n);
+               theta = 0;
     
-        if ismember(booleanOutputs, usedBool, 'rows') == 0
-           w = randn(n) * 1/sqrt(n);
-           theta = 0;
-        end
-    end
 
-    for epoch = 1:nEpochs
-        totalError = 0;
-        for mu = 2^dimension(n)
-            y = w(:,dimenstions(n))*booleanInputs(:,mu)-theta;
-            error = booleanOutputs(mu) - y;
+                for epoch = 1:nEpochs
+                    totalError = 0;
+                    for mu = 1:2^n
+                        b = 0;
+                        for i = 1:length(w)
+                            b = b + (w(i)*booleanInputs(i,mu)-theta);
+                        end
+                        y = sign(b);
+                        error = booleanOutputs(mu)-y;
+                       
+                        % update w and theta
+                        w = w + eta*(booleanOutputs(mu)-y).*booleanInputs(:,mu);
+                        theta = theta + (-eta)*(booleanOutputs(mu)-y);
+                        totalError = totalError + abs(error);
             
-            % update w and theta
-            w = w - eta*(t-y(mu))*booleanOutputs;
-            theta = theta - -eta*(t-y(mu));
-            totalError = totalError + abs(error);
-
-        end
-        if totalError == 0
-            counter = counter + 1;
-            break
+                    end
+                    if totalError == 0
+                        counter = counter + 1;
+                        break
+                    end
+                end
+              %add booleanOutputs to usedBool
+              usedBool(:,index) = booleanOutputs;
+              index = index + 1;
         end
     end
-    %add booleanOutputs to usedBool
-      usedBool(index) = booleanOutputs;
-      index = index + 1;
-end
+%end
