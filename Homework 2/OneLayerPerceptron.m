cb98014b5f2b9d47689b1dced9906236719d9d94
maximiliann97 <maximilian@live.se>
2022-09-26 20:29:38 +0200

    one layer perceptron correct

--- Homework 2/OneLayerPerceptron.m
+++ Homework 2/OneLayerPerceptron.m
@@ -7,21 +7,27 @@ validationSet = load('validation_set.csv');
 inputPattern = trainingSet(:,1:2);
 targets = trainingSet(:,3);
 
+validationPattern = validationSet(:,1:2);
+validationTargets = validationSet(:,3);
+
 % Center and std
 inputPatternCenter = (inputPattern - mean(inputPattern));
 inputPatternStd = std(inputPattern);
+validationCenter = (validationPattern - mean(validationPattern));
+validationSetStd = std(validationPattern);
 
 % Scaling
 inputPattern = inputPatternCenter./inputPatternStd;
+validationPattern = validationCenter./validationSetStd;
 
 
 % Intialize
 N = length(inputPattern);
-M1 = 8;     % Number of neurons in hidden layer
-pVal = height(validationSet);
+pVal = length(validationSet);
+M1 = 10;     % Number of neurons in hidden layer
 eta = 0.01;    % Learning rate
 errors = zeros(1,pVal);
-maxIterations = 5000;
+maxEpochs = 2000;
 
 
 % Initialize weights and thresholds
@@ -30,43 +36,59 @@ W2 = normrnd(0, 1, [1, M1]);
 thetaOne = zeros(M1,1);
 thetaTwo = 0;
 
-for k = 1:maxIterations
+for k = 1:maxEpochs
 %% Train perceptron
     for i = 1:N
         pattern = randi(N);
-        t = targets(pattern);
         x = inputPattern(pattern,1:2)';
+        t = targets(pattern);
         
+        
+        % Propagate
         V = tanh(LocalField(thetaOne,W1,x));
         O = tanh(LocalField(thetaTwo,W2,V));
+       
         
-        
-        
-        % Backpropagate error and update
+        % Compute errors for output layer
         delta = gPrime(LocalField(thetaTwo,W2,V)).*(t-O);
-        W2 = W2 + eta * delta.*V';
-        thetaTwo = thetaTwo - eta * delta;
+        outputErrorOne = eta * delta;
+
+        % Propagate backwards
+        delta = (delta*W2').*gPrime(LocalField(thetaOne,W1,x));
+        outputErrorTwo = eta * delta;
+
+        % Update weights and thresholds        
+        W1 = W1 + outputErrorTwo.*x';
+        W2 = W2 + outputErrorOne.*V';
+        thetaOne = thetaOne - outputErrorTwo;
+        thetaTwo = thetaTwo - outputErrorOne;
         
-        delta = delta.*W2*gPrime(LocalField(thetaOne,W1,x));
-        W1 = W1 + eta * delta.*x';
-        thetaOne = thetaOne - eta * delta;
+
+
     end
     
     %% Compute error with validation set
     output = zeros(pVal,1);
 
     for j = 1:pVal
-        xValidation = validationSet(j,1:2)';
+        xValidation = validationPattern(j,1:2)';
         vValidation = tanh(LocalField(thetaOne,W1,xValidation));
         output(j) = tanh(LocalField(thetaTwo,W2,vValidation));
     end
     
-    validationTargets = validationSet(:,3);
+
     C = ClassificationError(pVal,output,validationTargets);
-    fprintf('Classification error: %.3f Iteration: %.f\n',C,k)
+    errors(k) = C;
+    fprintf('Classification error: %.3f Epoch %.f\n',C,k)
     if C < 0.12
+        csvwrite('w1.csv', W1);
+        csvwrite('w2.csv', W2);
+        csvwrite('t1.csv', thetaOne);
+        csvwrite('t2.csv', thetaTwo);
         break
     end
 
 end
+
+
 
